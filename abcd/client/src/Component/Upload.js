ba313c8a11c3755f7a70a3656172adcec7fbb485
jaesung <jjs142458@naver.com>
2022-07-25 00:23:47 +0900

    input limit

--- abcd/client/src/Component/Upload.js
+++ abcd/client/src/Component/Upload.js
@@ -4,16 +4,28 @@ function Upload(props) {
   const [Content, setContent] = useState("");
 
   const onSubmit = () => {
-    let tempArr = [...props.ContentList, Content];
+    if (Content.length > 0) {
+      props.setContentList([...props.ContentList, Content]);
+      setContent("");
+    } else {
+      alert("1글자 이상입력해주세요.");
+    }
+  };
 
-    props.setContentList([...tempArr]);
-    setContent("");
+  const handleOnKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (Content.length > 0) {
+        onSubmit(); // Enter 입력이 되면 클릭 이벤트 실행
+      } else {
+        alert("1글자 이상입력해주세요.");
+      }
+    }
   };
 
   useEffect(() => {}, []);
 
   return (
-    <div>
+    <div onKeyPress={handleOnKeyPress}>
       <input
         type="text"
         value={Content}
