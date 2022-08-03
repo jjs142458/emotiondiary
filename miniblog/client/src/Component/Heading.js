004c3cce68bbf3a61542db091348cfb763c95c50
jaesung <jjs142458@naver.com>
2022-08-03 22:51:35 +0900

    design fix

--- miniblog/client/src/Component/Heading.js
+++ miniblog/client/src/Component/Heading.js
@@ -20,45 +20,53 @@ function Heading() {
       variant="white"
       style={{ borderBottom: "1px solid #ddd", paddingBottom: "11px" }}
     >
-      <Container>
-        <Navbar.Brand href="/" style={{ fontSize: "24px", fontWeight: "bold" }}>
-          I'm SoSorry's blog
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav
-            className="me-auto"
-            style={{ fontSize: "20px", paddingTop: "11px" }}
-          >
-            <Nav.Link as={Link} to="/">
-              home
-            </Nav.Link>
-            <Nav.Link as={Link} to="/upload">
-              upload
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Collapse
-          className="justify-content-end"
-          style={{
-            color: "dark",
-            textDecoration: "none",
-            cursor: "pointer",
-            fontSize: "20px",
-            paddingTop: "7px",
-          }}
+      <Navbar.Brand
+        href="/"
+        style={{
+          fontSize: "24px",
+          fontWeight: "bold",
+          marginLeft: "100px",
+        }}
+      >
+        I'm SoSorry's blog
+      </Navbar.Brand>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav
+          className="me-auto"
+          style={{ fontSize: "20px", paddingTop: "11px" }}
         >
-          {user.accessToken === "" ? (
-            <Nav.Link as={Link} to="/login">
-              Login
-            </Nav.Link>
-          ) : (
-            <Navbar.Text onClick={LoginOutHandler} style={{ color: "black" }}>
-              Logout
-            </Navbar.Text>
-          )}
-        </Navbar.Collapse>
-      </Container>
+          <Nav.Link as={Link} to="/" style={{ paddingLeft: "20px" }}>
+            home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/upload" style={{ paddingLeft: "20px" }}>
+            upload
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Navbar.Collapse
+        className="justify-content-end"
+        style={{
+          color: "dark",
+          textDecoration: "none",
+          cursor: "pointer",
+          fontSize: "20px",
+          paddingTop: "7px",
+          paddingLeft: "20px",
+          marginRight: "110px",
+        }}
+      >
+        {user.accessToken === "" ? (
+          <Nav.Link as={Link} to="/login">
+            Login
+          </Nav.Link>
+        ) : (
+          <Navbar.Text onClick={LoginOutHandler} style={{ color: "black" }}>
+            Logout
+          </Navbar.Text>
+        )}
+      </Navbar.Collapse>
     </Navbar>
   );
 }
